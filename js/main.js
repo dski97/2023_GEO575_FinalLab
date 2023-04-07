3ed06a5a4703b8078b3a1d0a6c963aabd48490f2
DOMINIC CWAL <dcwalinski97@gmai.com>
2023-04-07 17:39:07 -0400

    some changes to the js and adding basemaps

--- js/main.js
+++ js/main.js
@@ -2,12 +2,34 @@ document.addEventListener('DOMContentLoaded', function() {
     // create map
     var map = L.map('map').setView([37.8, -96], 4);
 
-    // add tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        // create basemap layers
+    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
         minZoom: 2,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    });
+
+    var cartoLight = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        maxZoom: 18,
+        minZoom: 2,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    });
+
+    var stamenToner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png', {
+        maxZoom: 18,
+        minZoom: 2,
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    // add default basemap to the map
+    osm.addTo(map);
+
+    // create an object containing all basemaps
+    var baseMaps = {
+        "OpenStreetMap": osm,
+        "Carto Light": cartoLight,
+        "Stamen Toner": stamenToner
+    };
 
     // function to load GeoJSON data and return the layer
     function loadGeoJSON(url) {
@@ -19,16 +41,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // create layer control
     var overlayLayers = {};
 
-    // load Passenger train stations GeoJSON file
-    loadGeoJSON('data/Stations.geojson').then(layer => {
-        overlayLayers["Stations"] = layer;
-    });
-
-    // load MainActiveTrainLines geojson file
-    loadGeoJSON('data/MainLineActiveTrainLines.geojson').then(layer => {
-        overlayLayers["Main Line Active Train Lines"] = layer;
-    });
-
     // load TrainAccidents geojson file and add it to a marker cluster group
     loadGeoJSON('data/TrainAccidents.geojson').then(data => {
         var trainAccidentsCluster = L.markerClusterGroup();
@@ -36,15 +48,36 @@ document.addEventListener('DOMContentLoaded', function() {
         overlayLayers["Train Accidents"] = trainAccidentsCluster;
     });
 
-    // wait for all GeoJSON layers to load before adding the layer control
+   // load MainActiveTrainLines geojson file
+   loadGeoJSON('data/MainLineActiveTrainLines.geojson').then(layer => {
+        overlayLayers["Main Line Active Train Lines"] = layer;
+    });
+    
+    // load Passenger train stations GeoJSON file
+    loadGeoJSON('data/Stations.geojson').then(layer => {
+        overlayLayers["Stations"] = layer;
+    });
+
+     // wait for all GeoJSON layers to load before adding the layer control
     Promise.all([
-        loadGeoJSON('data/Stations.geojson'),
-        loadGeoJSON('data/MainLineActiveTrainLines.geojson'),
-        loadGeoJSON('data/TrainAccidents.geojson')
-    ]).then(layers => {
+        loadGeoJSON('data/TrainAccidents.geojson').then(data => {
+            var trainAccidentsCluster = L.markerClusterGroup();
+            data.eachLayer(layer => trainAccidentsCluster.addLayer(layer));
+            overlayLayers["Train Accidents"] = trainAccidentsCluster;
+        }),
+        loadGeoJSON('data/MainLineActiveTrainLines.geojson').then(layer => {
+            overlayLayers["Main Line Active Train Lines"] = layer;
+        }),
+        loadGeoJSON('data/Stations.geojson').then(layer => {
+            overlayLayers["Stations"] = layer;
+        })
+    ]).then(() => {
         // create and add layer control to the map
         L.control.layers(null, overlayLayers).addTo(map);
     }).catch(error => {
         console.error('Error loading GeoJSON:', error);
     });
+    
+    // create and add basemap control to the map
+    var baseMapControl = L.control.layers(baseMaps, null, { position: 'bottomleft', collapsed: false }).addTo(map);
 });
